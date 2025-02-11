1fb9a04b6916aeca78793db38cc66289d1ab43f6
Chris Campbell <cacampbell@ucdavis.edu>
2025-02-11 11:05:16 -0800

    wip: start API code
    Signed-off-by: Chris Campbell <cacampbell@ucdavis.edu>

---
+++ api.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+type Response<T> = {
+    success: boolean;
+    content: T;
+    status?: number;
+}
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+export type { Response }
+export default instance
